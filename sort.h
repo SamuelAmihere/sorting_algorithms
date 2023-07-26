4f9eddac10d754400a7207fcd86d82d4c26efd30
SamuelAmihere <amisam2000@gmail.com>
2023-07-25 22:28:38 -0700

    quick sort

--- sort.h
+++ sort.h
@@ -30,5 +30,6 @@ void swap(int *x, int *y);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif

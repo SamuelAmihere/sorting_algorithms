ad43e38d175febe1ec49c728ca8e758e0aadb51a
SamuelAmihere <amisam2000@gmail.com>
2023-07-29 23:09:21 -0700

    heap sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,34 +1,36 @@
 #include "sort.h"
 
 /**
-* create_listint - Creates a doubly linked list from an array of integers
-*
-* @array: Array to convert to a doubly linked list
-* @size: Size of the array
-*
-* Return: Pointer to the first element of the created list. NULL on failure
+* heapify - Builds a heap from an array
+* 
+* @array: array to sort
+* @size: array size
+* @i: index
+* @size2: size of the array
+* 
+* Return: Nothing
 */
-listint_t *create_listint(const int *array, size_t size)
+void heapify(int *array, size_t size, size_t i, size_t size2)
 {
-listint_t *list;
-listint_t *node;
-int *tmp;
+size_t largest = i;
+size_t left = 2 * i + 1;
+size_t right = 2 * i + 2;
+int temp;
 
-list = NULL;
-while (size--)
+if (left < size && array[left] > array[largest])
+largest = left;
+
+if (right < size && array[right] > array[largest])
+largest = right;
+
+if (largest != i)
 {
-node = malloc(sizeof(*node));
-if (!node)
-return (NULL);
-tmp = (int *)&node->n;
-*tmp = array[size];
-node->next = list;
-node->prev = NULL;
-list = node;
-if (list->next)
-list->next->prev = list;
+temp = array[i];
+array[i] = array[largest];
+array[largest] = temp;
+print_array(array, size2);
+heapify(array, size, largest, size2);
 }
-return (list);
 }
 
 /**
